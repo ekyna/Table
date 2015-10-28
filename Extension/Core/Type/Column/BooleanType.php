3f9eb8ff017138a473b8228eb854b3628e195a23
Etienne Dauvergne <contact@ekyna.com>
2015-10-28 08:13:25 +0100

    [Table] Boolean column : added disable_property_path.

--- Extension/Core/Type/Column/BooleanType.php
+++ Extension/Core/Type/Column/BooleanType.php
@@ -20,23 +20,25 @@ class BooleanType extends PropertyType
     {
         parent::configureOptions($resolver);
         $resolver->setDefaults(array(
-            'true_label'  => 'ekyna_core.value.yes',
-            'false_label' => 'ekyna_core.value.no',
-            'true_class'  => 'label-success',
-            'false_class' => 'label-danger',
-            'route_name'  => null,
-            'route_parameters' => array(),
-            'route_parameters_map' => array(),
+            'true_label'            => 'ekyna_core.value.yes',
+            'false_label'           => 'ekyna_core.value.no',
+            'true_class'            => 'label-success',
+            'false_class'           => 'label-danger',
+            'route_name'            => null,
+            'route_parameters'      => array(),
+            'route_parameters_map'  => array(),
+            'disable_property_path' => null,
         ));
         $resolver->setRequired(array('route_name'));
         $resolver->setAllowedTypes(array(
-            'true_label'  => 'string',
-            'false_label' => 'string',
-            'true_class'  => 'string',
-            'false_class' => 'string',
-            'route_name'  => array('null', 'string'),
-            'route_parameters' => 'array',
-            'route_parameters_map' => 'array',
+            'true_label'            => 'string',
+            'false_label'           => 'string',
+            'true_class'            => 'string',
+            'false_class'           => 'string',
+            'route_name'            => array('null', 'string'),
+            'route_parameters'      => 'array',
+            'route_parameters_map'  => 'array',
+            'disable_property_path' => array('null', 'string'),
         ));
     }
 
@@ -47,15 +49,26 @@ class BooleanType extends PropertyType
     {
         parent::buildViewCell($cell, $table, $options);
 
-        $parameters = $options['route_parameters'];
-        foreach ($options['route_parameters_map'] as $key => $propertyPath) {
-            $parameters[$key] = $table->getCurrentRowData($propertyPath);
+        $route = $options['route_name'];
+        if (null !== $disablePath = $options['disable_property_path']) {
+            if ($table->getCurrentRowData($disablePath)) {
+                $route = null;
+            }
+        }
+
+        if (null !== $route) {
+            $parameters = $options['route_parameters'];
+            foreach ($options['route_parameters_map'] as $key => $propertyPath) {
+                $parameters[$key] = $table->getCurrentRowData($propertyPath);
+            }
+        } else {
+            $parameters = [];
         }
 
         $cell->setVars(array(
             'label'      => $cell->vars['value'] ? $options['true_label'] : $options['false_label'],
             'class'      => $cell->vars['value'] ? $options['true_class'] : $options['false_class'],
-            'route'      => $options['route_name'],
+            'route'      => $route,
             'parameters' => $parameters,
         ));
     }
