bddb01d6b53121ef392cdf54b7274b604a61fd3a
Étienne Dauvergne <contact@ekyna.com>
2024-05-02 12:56:49 +0200

    [Table] Boolean column: display null value.

--- Extension/Core/Type/Column/BooleanType.php
+++ Extension/Core/Type/Column/BooleanType.php
@@ -22,8 +22,11 @@ class BooleanType extends AbstractColumnType
 {
     public function buildCellView(CellView $view, ColumnInterface $column, RowInterface $row, array $options): void
     {
-        $view->vars['label'] = $view->vars['value'] ? $options['true_label'] : $options['false_label'];
-        $view->vars['class'] = $view->vars['value'] ? $options['true_class'] : $options['false_class'];
+        list($view->vars['label'], $view->vars['class']) = match ($view->vars['value']) {
+            null    => [$options['null_label'], $options['null_class']],
+            true    => [$options['true_label'], $options['true_class']],
+            default => [$options['false_label'], $options['false_class']],
+        };
 
         if (isset($view->vars['path']) || (isset($view->vars['route']) && isset($view->vars['parameters']))) {
             return;
@@ -48,23 +51,33 @@ class BooleanType extends AbstractColumnType
             }
         }
 
-        $view->vars['route']      = $options['route'];
+        $view->vars['route'] = $options['route'];
         $view->vars['parameters'] = $parameters;
     }
 
     public function export(ColumnInterface $column, RowInterface $row, array $options): ?string
     {
-        return $row->getData($column->getConfig()->getPropertyPath())
-            ? $options['true_label']
-            : $options['false_label'];
+        $value = $row->getData($column->getConfig()->getPropertyPath());
+
+        if (null === $value) {
+            return $options['null_label'];
+        }
+
+        if (true === $value) {
+            return $options['true_label'];
+        }
+
+        return $options['false_label'];
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver
             ->setDefaults([
+                'null_label'            => 'Null',
                 'true_label'            => 'Yes',
                 'false_label'           => 'No',
+                'null_class'            => 'label-default',
                 'true_class'            => 'label-success',
                 'false_class'           => 'label-danger',
                 'route'                 => null,
@@ -72,8 +85,10 @@ class BooleanType extends AbstractColumnType
                 'parameters_map'        => [],
                 'disable_property_path' => null,
             ])
+            ->setAllowedTypes('null_label', 'string')
             ->setAllowedTypes('true_label', 'string')
             ->setAllowedTypes('false_label', 'string')
+            ->setAllowedTypes('null_class', 'string')
             ->setAllowedTypes('true_class', 'string')
             ->setAllowedTypes('false_class', 'string')
             ->setAllowedTypes('route', ['null', 'string'])
